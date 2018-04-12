e425a9624639bb80706dc2c1473e2a2953e0047e
Steven Chennault <schenn@gmail.com>
2018-04-11 20:01:02 -0700

    Remove unused and non-existent imports from earlier practice

--- components/standard.js
+++ components/standard.js
@@ -1,6 +1,3 @@
-import {Heading} from './cards/examples/Heading.js';
-import {UserInput} from './cards/examples/UserInput.js';
-import {UserNumeric} from './cards/examples/UserNumeric.js';
 
 // Default Components which can be used by Cards.
 // @todo Come up with some Default Components?
