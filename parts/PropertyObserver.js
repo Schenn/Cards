870b1aa263be597bea1ede8c958b0a14feba84d6
Steven Chennault <schenn@gmail.com>
2018-05-06 17:35:51 -0700

    PropertyObserver Uses InnerText As Bound Function Script
    Now when you create a property observer, you specify the property to
    observe with the "property" attribute and write your callback in the
    body of the element.
    
    The element's callback is memoized so it cannot be changed after it's
    been created the first time.
    
    The callback is bound to the context of the Card and has access to the
    following scoped variables,
    - value - the new property value
    - component - The Custom Element which wraps the component class.
    
    Much Nicer!

--- parts/PropertyObserver.js
+++ parts/PropertyObserver.js
@@ -2,74 +2,64 @@ import {Part} from './Part.js';
 import {Observation} from '../utilities/Observation.js';
 
 /**
- * @todo Clean Up. Callbacks memoization here seems redundant.
+ * Memoize the callbacks to prevent observers from having their scripts changed after their first creation.
+ * @type {WeakMap}
  */
+let callbacks = new WeakMap();
 
-/**
- * Card Level Callbacks to trigger on component attribute change.
- * @type {{}}
- */
-let callbacks = {};
+let unique = 0;
 
 /**
  * Property Observer monitors it's parent component's attributes for changes and when
  *  a matching change is detected, it triggers an appropriate callback on the Card which
  *  added the Property Observer to the component.
  *
- *  @todo Use a similar flow as the parentComponent query to get the card element to trigger effects on.
+ * @todo Is there any way to identify the inner text of this element as js for IDEs?
+ *      Or, should we use a script tag as a child and grab the text from that?
+ *      Maybe once custom-elements are more fully implemented, we can extend the script tag.
+ *      For now, one can only create new elements, not extend existing classical elements.
  */
 export class PropertyObserver extends Part {
+
+  constructor(){
+    super();
+    this.__unique = unique++;
+  }
 
   /**
    * When connected to the DOM, start observing the parent component's attributes
    *  for the changes specified in the data-onchange attribute of this node.
+   *
+   *  Callback is bound to the context of the containing card (as that's where the callbacks should be)
+   *  New Property Value is passed into the callback function under the "value" argument.
+   *  Component is passed as the "component" argument. (So that the changed component is identifiable)
+   *
+   *  Best Use Case is to identify a card function to trigger when a property is changed,
+   *      not writing out complex functionality inside the element.
    */
   connectedCallback(){
+    if(typeof callbacks[this.__unique] === "undefined"){
+      // Get the script from the inner text of this element.
+      let script = this.innerText;
+      // Get the inner text of this node, convert it to a function and erase the innertext value.
+      // While not a great practice, the benefit to doing Function like this instead of "eval"
+      //  is the ability to control the context and scope of the function script.
+      callbacks[this.__unique] = Function("value", "component", script);
+      // Clear the innertext as it shouldn't render.
+      this.innerText = '';
+    }
+
     let comp = this.parentComponent();
-    if(!this.dataset.hasOwnProperty("onchange")){
-      throw "Property Observer has no properties to observe?";
-    }
-
-    callbacks[this.__id] = {};
-    let props = Object.keys(this.dataset.onchange);
-
-    // If properties have been specified in the data-onchange attribute (propertyName: cardFunctionName)
-    if(props.length > 0){
-      let observation = new Observation();
-      for(let prop of props){
-        // On attribute Change, if there's a matching callback, trigger it.
-        observation.onAttributeChange(prop, (val)=>{
-          if(callbacks[this.__id].hasOwnProperty(prop)){
-            callbacks[this.__id][prop](val);
-          }
-        });
-      }
-
-      observation.observe(comp);
-
-    }
+    let card = comp.card;
+    // REQUIRED
+    let property = this.attributes.property.value;
+    let cb = callbacks[this.__unique].bind(card);
+    // Trigger the callback on attribute change.
+    let observation = new Observation();
+    observation.onAttributeChange(property, (val)=>{
+      // Bind the callback function to the parent card before calling.
+      cb(val, comp);
+    });
+    observation.observe(comp);
   }
-
-  /**
-   * Set the card to fire callbacks on.
-   * Called from the Cards App after the card is created.
-   *
-   * @param card
-   * @todo Now that Cards are custom-elements, use a similar flow as the parentComponent query to find the containing card to message about the change.
-   */
-  setCardCallbacks(card){
-    let props = Object.keys(this.dataset.onChange);
-    for(let prop of props){
-      let cb = this.dataset[prop];
-      if(cb.indexOf(".") > -1){
-        // Callback function is a component message.
-        let {compName, funcName} = cb.split(".");
-        callbacks[this.__id] = card.getComponent(compName)[funcName];
-      } else {
-        // Callback function is a function on the card itself.
-        callbacks[this.__id] = card[funcName];
-      }
-    }
-  }
-
 }
