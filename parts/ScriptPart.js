53bcf3f33d21c9ea1c5e37560a4befa50f7b4e3a
Steven Chennault <schenn@gmail.com>
2018-05-07 12:34:11 -0700

    Script Part documentation and cleanup
    Now the makescript function expects null (the result of getAttribute
    when the attribute is missing) to indicate not to use custom arguments.

--- parts/ScriptPart.js
+++ parts/ScriptPart.js
@@ -1,4 +1,4 @@
-import {Part} from './Part';
+import {Part} from './Part.js';
 
 let unique = 0;
 let callbacks = new WeakMap();
@@ -10,6 +10,8 @@ let callbacks = new WeakMap();
  * This only prepares the function. Your App should execute it when its appropriate.
  *
  * argument attribute - the name of the argument to pass into the scope of the script part element.
+ *
+ * @todo Use spread to fill out the arguments for the Function creation. Thus allowing any number of custom arguments instead of one.
  */
 export class ScriptPart extends Part {
   constructor(){
@@ -17,36 +19,71 @@ export class ScriptPart extends Part {
     this.__unique = ++unique;
   }
 
+  /**
+   * Get the memoized function.
+   * @return {*}
+   */
   get script(){
     return callbacks[this.__unique];
   }
 
+  /**
+   * Has the script been prepared as a function yet?
+   *
+   * @return {boolean}
+   */
   scriptReady(){
     return typeof callbacks[this.__unique] !== "undefined";
   }
 
-  makeScript(scriptArg){
-    // Get the script from the inner text of this element.
+  /**
+   * Convert the innertext of the
+   * @param {null | string} scriptArg
+   */
+  makeScript(scriptArg = null){
+    if(this.scriptReady()){
+      return;
+    }
+
+    /** Get the inner text of this node, convert it to a function and erase the innertext value.
+     *  While not a great practice, the benefit to doing Function like this instead of "eval"
+     *    is the ability to control the context and scope of the function script.
+     */
     let script = this.innerText;
-    // Get the inner text of this node, convert it to a function and erase the innertext value.
-    // While not a great practice, the benefit to doing Function like this instead of "eval"
-    //  is the ability to control the context and scope of the function script.
-    callbacks[this.__unique] = Function(scriptArg, "component", script);
-    // Clear the innertext as it shouldn't render.
+    // If there's no custom argument to use, don't prepare the function to expect one.
+    if(scriptArg === null){
+      callbacks[this.__unique] = Function("component", script);
+    } else {
+      callbacks[this.__unique] = Function(scriptArg, "component", script);
+    }
+
+    // Clear the innertext as it shouldn't actually render.
     this.innerText = '';
   }
 
+  /**
+   * You should call this with super.connectedCallback to prepare the script.
+   */
   connectedCallback(){
     if(!this.scriptReady()){
-      let scriptArg = this.getAttribute("argument");
-      this.makeScript(scriptArg);
+      this.makeScript(this.getAttribute("argument"));
     }
   }
 
+  /**
+   * Run the context scoped script
+   *
+   * @param {*} argument
+   */
   execute(argument){
     let comp = this.parentComponent();
     let card = comp.card;
     let cb = this.script.bind(card);
-    cb(argument, comp);
+    if(this.getAttribute("argument") !== null){
+      cb(argument, comp);
+    } else {
+      cb(comp);
+    }
+
   }
 }
